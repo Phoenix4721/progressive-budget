81a5c90b691a3766f6b3b099f63861b89980a528
Chronos <jjboswell00@gmail.com>
2020-04-30 16:56:41 -0400

    done

--- develop/public/database.js
+++ develop/public/database.js
@@ -19,13 +19,10 @@ request.onerror = function (event) {
 };
 
 function saveRecord(record) {
-    // create a transaction on the pending db with readwrite access
     const transaction = db.transaction(["pending"], "readwrite");
 
-    // access your pending object store
     const store = transaction.objectStore("pending");
 
-    // add record to your store with add method.
     store.add(record);
 }
 
@@ -55,5 +52,4 @@ function checkDatabase() {
     };
 }
 
-// listen for app coming back online
 window.addEventListener("online", checkDatabase);
--- develop/public/serviceWorker.js
+++ develop/public/serviceWorker.js
@@ -20,21 +20,17 @@ self.addEventListener('install', function (event) {
 });
 
 
-//This runs every time the fetch function is called in our project
+
 self.addEventListener('fetch', function (event) {
-    //http://localhost:3000
-    //index.html
 
     if (event.request.clone().method === 'GET') {
         event.respondWith(
-            caches.match(event.request) //identifying the files we need based of off the url
+            caches.match(event.request)
                 .then(function (response) {
                     if (response) {
                         console.log('Network request for ', event.request.url);
                         return response;
                     }
-
-                    //getting the files
                     return fetch(event.request).then(function (response) {
                         return caches.open(cacheName).then(function (cache) {
                             cache.put(event.request.url, response.clone());
